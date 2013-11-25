bee179e0a3a011401667748aac4a2eaad99dbce2
Walther <veeti.haapsamo@gmail.com>
2013-11-25 15:24:47 +0200

    cleanup, software install init

--- bash/.bash_aliases
+++ bash/.bash_aliases
@@ -1,5 +1,5 @@
 alias ls='ls -CF --color=auto'
-    
+
 function cd()
 {
     builtin cd "$*" && ls
---
+++ software.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# My commonly used software, in a nice single apt-get install
+
+sudo apt-get install darktable gimp git htop vim
