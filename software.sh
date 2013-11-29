1a6cb6250bd00830db4e27a0db8090bba51deb01
Walther <veeti.haapsamo@gmail.com>
2013-11-29 16:43:13 +0200

    add lightdm to software install; get a 'desktop environment' with gtk etc

--- software.sh
+++ software.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # My commonly used software, in a nice single apt-get install
 
-sudo apt-get install git htop vim rxvt-unicode-256color
+sudo apt-get install git htop vim rxvt-unicode-256color lightdm
