59792ce2941f6242a6f1d5a75d4e4c640cb2d213
Walther <veeti.haapsamo@gmail.com>
2013-11-29 20:06:41 +0200

    add chromium, alphabetic order

--- software.sh
+++ software.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # My commonly used software, in a nice single apt-get install
 
-sudo apt-get install git htop vim rxvt-unicode-256color xclip lightdm i3 i3-wm i3status
+sudo apt-get install chromium git htop i3 i3-wm i3status lightdm rxvt-unicode-256color vim xclip
