e8a3ef8022bf9bcc6223c72bd0a5d0e65259d2e8
Walther <veeti.haapsamo@gmail.com>
2013-11-29 17:49:32 +0200

    xclip

--- software.sh
+++ software.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # My commonly used software, in a nice single apt-get install
 
-sudo apt-get install git htop vim rxvt-unicode-256color lightdm
+sudo apt-get install git htop vim rxvt-unicode-256color xclip lightdm i3 i3-wm i3status
