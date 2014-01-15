5cd01e02c6ead898c20f6deb4247efdef2d70acd
Walther <veeti.haapsamo@gmail.com>
2014-01-15 16:17:00 +0200

    add fish shell to install list

--- software.sh
+++ software.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # My commonly used software, in a nice single apt-get install
 
-sudo apt-get install chromium ghc git htop i3 i3-wm i3status lightdm rxvt-unicode-256color vim xclip
+sudo apt-get install chromium fish ghc git htop i3 i3-wm i3status lightdm rxvt-unicode-256color vim xclip
