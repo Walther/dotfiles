dd9909867bfa3fa099b19b4831d7ae9ff18469c9
Walther <veeti.haapsamo@gmail.com>
2014-02-13 00:10:41 +0200

    software -> install, tweaks, automation - note, untested!

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# My commonly used software, in a nice single apt-get install
+
+sudo apt-get install fish ghc git htop i3 i3-wm i3status lightdm nmap python-pip rxvt-unicode-256color vim xclip
+
+pip install --user git+git://github.com/Lokaltog/powerline
+
+cp Xdefaults ~/.Xdefaults
+cp Xmodmap ~/.Xmodmap
+cp screenrc ~/.screenrc
+cp tmux.conf ~/.tmux.conf
+
+mkdir -p ~/.config/fish/
+cp config.fish ~/.config/fish/config.fish
+
+mkdir -p ~/.vim/colors/
+cp monokai-vim/colors/monokai.vim ~/.vim/colors/
+
+mkdir ~/.i3/
+cp i3/* ~/.i3/
+
--- software.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# My commonly used software, in a nice single apt-get install
-
-sudo apt-get install chromium fish ghc git htop i3 i3-wm i3status lightdm rxvt-unicode-256color vim xclip
