73afff326112944098085fccf58dcf3a919e3a12
Walther <veeti.haapsamo@gmail.com>
2014-03-03 13:41:19 +0200

    separate sudo-requiring install script and user-only file copying script

---
+++ files.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Makes necessary dirs and puts the dotfiles in place.
+
+cp Xdefaults ~/.Xdefaults
+cp Xmodmap ~/.Xmodmap
+cp screenrc ~/.screenrc
+cp tmux.conf ~/.tmux.conf
+cp gitconfig ~/.gitconfig
+
+mkdir -p ~/.config/fish/
+cp config.fish ~/.config/fish/config.fish
+
+mkdir -p ~/.vim/colors/
+cp vim-monokai/colors/monokai.vim ~/.vim/colors/
+cp vimrc ~/.vimrc
+
+
+mkdir -p ~/.i3/
+cp i3/* ~/.i3/
+
+mkdir -p ~/.fonts/
+cp -R powerline-fonts/ ~/.fonts/
+fc-cache -vf ~/.fonts/
+
+git clone git://github.com/bpinto/oh-my-fish.git ~/.oh-my-fish
--- install.sh
+++ install.sh
@@ -1,36 +1,15 @@
 #!/bin/bash
-# My dotfiles - essential software, their configs, etc
+# My preferred software + some customizations. Requires sudo.
 
 sudo apt-get update;
-sudo apt-get install fish ghc git htop i3 i3-wm i3status lightdm nmap python-pip rxvt-unicode-256color tmux vim vim-gtk xclip;
+sudo apt-get install fish ghc git htop i3 i3-wm i3status lightdm nmap python-pip rxvt-unicode-256color tmux vim vim-nox;
+
+sudo cp urxvt-perls/clipboard /usr/lib/urxvt/perl/
+
+pip install --user git+git://github.com/Lokaltog/powerline
 
 mkdir -p ~/Downloads
 cd ~/Downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
-pip install --user git+git://github.com/Lokaltog/powerline
-
-cp Xdefaults ~/.Xdefaults
-cp Xmodmap ~/.Xmodmap
-cp screenrc ~/.screenrc
-cp tmux.conf ~/.tmux.conf
-cp gitconfig ~/.gitconfig
-
-mkdir -p ~/.config/fish/
-cp config.fish ~/.config/fish/config.fish
-
-mkdir -p ~/.vim/colors/
-cp vim-monokai/colors/monokai.vim ~/.vim/colors/
-cp vimrc ~/.vimrc
-
-sudo cp urxvt-perls/clipboard /usr/lib/urxvt/perl/
-
-mkdir -p ~/.i3/
-cp i3/* ~/.i3/
-
-mkdir -p ~/.fonts/
-cp -R powerline-fonts/ ~/.fonts/
-fc-cache -vf ~/.fonts/
-
-git clone git://github.com/bpinto/oh-my-fish.git ~/.oh-my-fish
