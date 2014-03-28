48da2405a74ec502840ecc82f9477841c8664854
Walther <veeti.haapsamo@gmail.com>
2014-03-28 16:37:09 +0200

    cleanup III

--- files.sh
+++ files.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
-# Makes necessary dirs and puts the dotfiles in place.
+# Makes necessary dirs, clones things from github, and puts the dotfiles in place.
 
+# Various essentials
 cp Xdefaults ~/.Xdefaults
 cp Xmodmap ~/.Xmodmap
 cp screenrc ~/.screenrc
 cp tmux.conf ~/.tmux.conf
 cp gitconfig ~/.gitconfig
 
+# Shell
 mkdir -p ~/.config/fish/
 cp config.fish ~/.config/fish/config.fish
+git clone git://github.com/bpinto/oh-my-fish.git ~/.oh-my-fish;
 
+# Vim
+mkdir -p /tmp/vim-molokai
+git clone https://github.com/tomasr/molokai /tmp/vim-molokai
 mkdir -p ~/.vim/colors/
-cp molokai.vim ~/.vim/colors/
+cp /tmp/vim-molokai/colors/molokai.vim ~/.vim/colors/
 cp vimrc ~/.vimrc
 
-
+git clone https://github.com/bling/vim-airline /tmp/vim-airline;
+cp -R /tmp/vim-airline/doc /tmp/vim-airline/plugin /tmp/vim-airline/t /tmp/vim-airline/autoload ~/.vim/
 mkdir -p ~/.i3/
 cp i3* ~/.i3/
 
+# Fonts
 mkdir -p ~/.fonts/
 mkdir -p ~/tmp/powerline-fonts
 git clone https://github.com/Lokaltog/powerline-fonts /tmp/powerline-fonts
 cp -R /tmp/powerline-fonts/ ~/.fonts/
 fc-cache -vf ~/.fonts/
 
-git clone git://github.com/bpinto/oh-my-fish.git ~/.oh-my-fish;
-git clone https://github.com/bling/vim-airline /tmp/vim-airline;
-cp -R /tmp/vim-airline/doc /tmp/vim-airline/plugin /tmp/vim-airline/t /tmp/vim-airline/autoload ~/.vim/
