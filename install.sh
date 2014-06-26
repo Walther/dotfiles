8743e7eeead889c46a54cdf344df415d3e7b63d9
Walther <veeti.haapsamo@gmail.com>
2014-06-26 14:45:36 +0300

    fix update-alternatives syntax for dmenu.xft

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt-get update;
 sudo apt-get install feh fish ghc git htop i3 i3-wm iotop lightdm nethogs nmap rxvt-unicode-256color scrot tmux vim-nox xcompmgr;
 
 # update dmenu to use the xft version. Sidenote, why wasn't this feature merged but put as an alternative?
-sudo update-alternatives dmenu /usr/bin/dmenu.xft;
+sudo update-alternatives --set dmenu /usr/bin/dmenu.xft;
 
 mkdir -p /tmp/urxvt-perls
 git clone https://github.com/muennich/urxvt-perls /tmp/urxvt-perls
