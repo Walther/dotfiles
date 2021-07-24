96805c5efdbeb81a57ce6e868dba78ace40fd760
Walther <veeti.haapsamo@gmail.com>
2021-07-24 18:32:41 +0300

    Add cargo-install.sh script

---
+++ cargo-install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -exuo pipefail
+cargo install \
+    bat \
+    bottom \
+    cargo-edit \
+    cargo-update \
+    cargo-watch \
+    flamegraph \
+    git-delta \
+    just \
+    tokei \
+    topgrade \
+    viu \
+
