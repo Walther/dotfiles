6ca9eda0ce82cdb5b0ee63dc25ae64ae1a502537
Walther <veeti.haapsamo@gmail.com>
2022-01-08 14:55:26 +0200

    Move homebrew installation into a script

---
+++ homebrew.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -exuo pipefail
+brew install \
+  awscli \
+  coreutils \
+  duf \
+  exiftool \
+  ffmpeg \
+  figlet \
+  ghostscript \
+  git \
+  heroku/brew/heroku \
+  htop \
+  httpie \
+  jq \
+  libvterm \
+  mactex-no-gui \
+  mosh \
+  neovim \
+  nghttp2 \
+  nmap \
+  pandoc \
+  teensy_loader_cli \
+  tmux \
+  tree \
+  tty-clock \
+  watch \
