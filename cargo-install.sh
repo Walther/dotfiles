7abb7d7393535baf9f2753abd1d8a9fe4db4e1a6
Walther <email+git@waltteri.net>
2024-10-28 19:14:19 +0200

    cargo-binstall: add self-install

--- cargo-install.sh
+++ cargo-install.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -exuo pipefail
-cargo binstall \
+
+if ! command -v cargo-binstall 2>&1 >/dev/null
+then
+    echo "cargo binstall not found, installing"
+    curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+fi
+
+cargo binstall --disable-telemetry \
     bat \
     bottom \
     cargo-edit \
